c889231009d1d2489bf74350985c77f6f67c3378
Jaswant Singh <>
2018-04-07 22:20:52 +0530

    First functional component - Header

---
+++ src/Components/header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+const Header = ()=>{
+    return  <header className="row application-header">
+                <div>header</div>
+            </header>
+}
+
+export default Header;
